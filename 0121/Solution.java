bdbfe18df8907cd88866dd83ca0f27a669c35b90
Aspirant Ant <837215230@qq.com>
2021-06-07 20:06:43 +0800

    java code

---
+++ 0121/Solution.java
@@ -0,0 +1,13 @@
+public int maxProfit(int[] prices) {
+    int len = prices.length;
+    int minPrice = prices[0];
+    int result = 0;
+    for (int i = 1; i < len; i++) {
+        if(prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > result) {
+            result = prices[i] - minPrice;
+        }
+    }
+    return result;
+}
